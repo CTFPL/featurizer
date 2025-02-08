06752e96d1a32579598760d388e24fe533c22e6f
Александр Юсов <aleksandryusov@MacBook-Pro-Aleksandr-7.local>
2025-02-09 01:13:03 +0300

    add ma,ema and tests

---
+++ src/features/average.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from typing import Sequence
+
+from src.data.asset import AssetFeatureCalculatorVector, AssetFeatureCalculatorIterable, AssetPriceStorage
+
+
+class MAFeatureCalculator(AssetFeatureCalculatorIterable):
+    def __init__(self, verbose: bool = False, override_features: bool = False, periods: int = 1, value_col: str = "close"):
+        super().__init__(verbose, override_features)
+        self.periods = periods
+        self.value_col = value_col
+        self.name = f"MA_{self.periods}"
+
+    def calculate_one_unit(self, asset_price_storage: AssetPriceStorage, dt: datetime) -> float | None:
+        df = asset_price_storage.get_slice_by_ticks(dt, n_ticks=self.periods)
+        
+        if df.shape[0] == 0:
+            return None
+        else:
+            return df[self.value_col].mean()
+
+
+class EMAFeatureCalculator(AssetFeatureCalculatorIterable):
+    def __init__(
+            self, 
+            verbose: bool = False, 
+            override_features: bool = False, 
+            periods: int = 1, 
+            smoothing: int = 2,
+            value_col: str = "close"
+        ):
+        super().__init__(verbose, override_features)
+        self.periods = periods
+        self.smoothing = smoothing
+        self.alpha = smoothing / (1 + self.periods)
+
+        self.value_col = value_col
+        self.name = f"EMA_{self.periods}_{self.smoothing:.5f}"
+
+        self.last_ema = None
+
+    def calculate_one_unit(self, asset_price_storage: AssetPriceStorage, dt: datetime) -> float | None:
+        # only true for first value
+        if self.last_ema is None:
+            self.last_ema = asset_price_storage.df[self.value_col][0]
+            return self.last_ema
+
+        current_price = asset_price_storage.get_slice_by_ticks(dt, n_ticks=1)
+        assert current_price.shape[0] == 1
+        current_price = current_price[self.value_col][0]
+        
+        value = self.alpha * current_price + (1 - self.alpha) * self.last_ema
+        self.last_ema = value
+
+        return value
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,25 @@ import pytest
 
 import polars as pl
 
+from src.features.average import MAFeatureCalculator
+from src.data.asset import AssetPriceStorage
 
-@pytest.fixture()
+
+@pytest.fixture(scope="session")
 def t_minute_data():
     return pl.read_parquet("./tests/static/T.parquet")
+
+
+@pytest.fixture()
+def asset_t(t_minute_data):
+    return AssetPriceStorage("T", t_minute_data)
+
+
+@pytest.fixture(scope="session")
+def ma_features(t_minute_data):
+    asset = AssetPriceStorage("T", t_minute_data)
+    ma_calc = MAFeatureCalculator(periods=120, verbose=False, override_features=True)
+    ma_calc.name = "ma"
+    ma_calc.calculate(asset)
+
+    return asset
