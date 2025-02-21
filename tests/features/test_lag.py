5e808cd0ffad703d19e9e691169cb33b23601ae9
Александр Юсов <aleksandryusov@MacBook-Pro-Aleksandr-7.local>
2025-02-21 20:50:10 +0300

    Add tests on lag featurizer

--- tests/features/test_lag.py
+++ tests/features/test_lag.py
@@ -5,12 +5,20 @@ from src.features.lag import LagTicksFeaturizer, LagTimeDeltaFeaturizer
 
 
 @pytest.mark.parametrize("lag", [0, 1, 10, 100])
-def test_lag(t_asset, lag):
+def test_lag_ticks_features(t_asset, lag):
     featurizer = LagTicksFeaturizer(
-        "lag_0", 
+        "lag", 
         feature="close",
         lag=lag,
         add_to_asset=True,
     )
     featurizer.get_features_batch(t_asset)
-    # assert t_asset.to_polars().select((pl.col("lag_0") == pl.col("close")).mean()) == 1
+
+    shifted_series = t_asset.to_polars().select(
+        (pl.col("lag") == pl.col("close").shift(lag)).alias("eq")
+    )
+
+    # проверка, что кол-во нулов совпадает с размером лага
+    assert shifted_series.null_count()["eq"][0] == lag
+    # проверка, что лаги совпадают
+    assert shifted_series.mean()["eq"][0] == 1
