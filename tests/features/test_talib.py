a54ef9c81ba83825e65caab435404df90e52dc6b
Александр Юсов <aleksandryusov@MacBook-Pro-Aleksandr-7.local>
2025-02-22 18:39:11 +0300

    add tests

--- tests/features/test_talib.py
+++ tests/features/test_talib.py
@@ -5,7 +5,7 @@ from src.features.talib import TALibFeaturizer
 from tests.features.base import base_test_featurizer_eq
 
 
-def test_talib(t_asset):
+def test_talib_ma(t_asset):
     featurizer = TALibFeaturizer(
         "ma_low", 
         func=lambda kwargs: talib.MA(real=kwargs["open"], timeperiod=360), 
@@ -21,8 +21,24 @@ def test_talib(t_asset):
     assert "ma_low" not in t_asset.to_polars().columns
 
 
+def test_talib_sma(t_asset):
+    featurizer = TALibFeaturizer(
+        "ma_low", 
+        func=lambda kwargs: talib.SMA(real=kwargs["open"], timeperiod=360), 
+        add_to_asset=True
+    )
+    featurizer.get_features_batch(t_asset)
+    
+    dd = t_asset.to_polars()
+    assert "ma_low" in dd.columns
+
+    t_asset.delete_field("ma_low")
+
+    assert "ma_low" not in t_asset.to_polars().columns
+
+
 @pytest.mark.parametrize("timeperiod", [1, 100])
-def test_talib_batch_eq_iter(t_asset_small, timeperiod):
+def test_talib_batch_eq_iter_ma(t_asset_small, timeperiod):
     asset = t_asset_small
     base_test_featurizer_eq(
         asset, 
@@ -34,6 +50,24 @@ def test_talib_batch_eq_iter(t_asset_small, timeperiod):
         TALibFeaturizer(
             "iter", 
             func=lambda kwargs: talib.MA(real=kwargs["open"], timeperiod=timeperiod), 
+            add_to_asset=True
+        )
+    )
+
+
+@pytest.mark.parametrize("timeperiod", [2, 100])
+def test_talib_batch_eq_iter_sma(t_asset_small, timeperiod):
+    asset = t_asset_small
+    base_test_featurizer_eq(
+        asset, 
+        TALibFeaturizer(
+            "batch", 
+            func=lambda kwargs: talib.SMA(real=kwargs["open"], timeperiod=timeperiod), 
+            add_to_asset=True
+        ),
+        TALibFeaturizer(
+            "iter", 
+            func=lambda kwargs: talib.SMA(real=kwargs["open"], timeperiod=timeperiod), 
             add_to_asset=True
         )
     )
